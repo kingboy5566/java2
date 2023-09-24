ac5a8d637b232e6b0c9e00b6dea7ef87c119fff5
muhammad45401 <saadjani3333@gmail.com>
2023-09-25 02:23:18 +0500

    java2

---
+++ java.js
@@ -0,0 +1,30 @@
+var  age="I am 32 year old"
+alert(age)
+
+var page =" You have visited site 14 times"
+alert(page)
+
+
+var birthyear ="My birth year 1991"
+alert(birthyear)
+
+
+var vName ="Muhammad Ilyas\n"
+var product ="Oderd 5 T-Shrit\n"
+var web ="Product by xyz\n"
+var fDatils =vName+product+web
+alert(fDatils)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
